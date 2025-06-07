2d4f2151567a6cb3e799aefb0ba5d8a5d7f250a0
Joyce-haoyu <133488785+Joyce-haoyu@users.noreply.github.com>
2025-06-07 09:04:06 +0800

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -8,9 +8,9 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com/China_Joyceyue",
+  github: "https://github.com/joycehaoyu",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://v.douyin.com/W3lyhzhqpHM/",
+  email: "joycehaoyu@gmail.com",
 };
